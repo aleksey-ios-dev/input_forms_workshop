57757220af7ec83d0d9958e9ca1fae6d0339ea8d
Aleksey <aleksey@MacBook-Pro-Aleksey.local>
2017-04-08 10:02:21 +0700

    user description

--- Workshop/User.swift
+++ Workshop/User.swift
@@ -15,4 +15,12 @@ class User {
     var age = 0
     var numberOfKids = 0
     
+    var description: String {
+        var result = "\(firstName) \(lastName)"
+        result.append("Age: \(age)\n")
+        result.append("Kids: \(numberOfKids)\n")
+        
+        return result
+    }
+    
 }
